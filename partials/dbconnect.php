2088da2f4fb5307812acd8123beefd015c072c2f
Prachi Sharma <62355777+Prachi9127-code@users.noreply.github.com>
2023-07-05 22:46:37 +0530

    dbconnect.php

--- partials/dbconnect.php
+++ partials/dbconnect.php
@@ -1,13 +1,18 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password ="";
-$database = "notes";
+$servername = "myserverdiscuss.mysql.database.azure.com";
+$username = "bhhjkfgtkk";
+$password ="Prachi12a";
+$database = "idiscuss-database";
 
-$conn = mysqli_connect($servername, $username, $password, $database);
+
+$conn = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL, "/Users/prachisharma/Downloads/DigiCertGlobalRootCA.crt (2).pem", NULL, NULL);
+mysqli_real_connect($conn, "myserverdiscuss.mysql.database.azure.com", $username, $password, $database, 3306, MYSQLI_CLIENT_SSL);
+// $conn = mysqli_connect($servername, $username, $password, $database);
 if(!$conn){
 //     echo "success";
 // }else{
     die("Error".mysqli_connect_error());
 }
 ?>
+
